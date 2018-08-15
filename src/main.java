f814482e6cee92e7b723a45353fe1e3dc6524e9c
ArthurBulyakov <arthur.bulyakov@mail.ru>
2018-08-15 16:07:40 +0300

    +

--- src/main.java
+++ src/main.java
@@ -1,1 +1,11 @@
-import java.awt.color.CMMException
+import com.google.gson.Gson;
+import com.google.gson.JsonIOExeption;
+import com.google.gson.JsonSyntaxExeption;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class main {
+
+}
